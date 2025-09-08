7bb1d740780015ff4d7a5db1afae7a4a506f8d0c
Kousha <150776406+KoushaZhiyani@users.noreply.github.com>
2025-09-08 11:55:28 +0330

    Add files via upload

---
+++ main-PREPROCESS-Return.py
@@ -0,0 +1,111 @@
+# main.py
+
+import pandas as pd
+from etl.config import read_table_from_sql, save_table_to_sql
+from etl.preprocess import preprocessing
+from etl.updater import check_update, merge_df_func
+from etl.customer_utils import check_customer_id, get_visitor_id
+from etl.check_count_flag import check_count_flag
+from etl.log import write_massege
+from etl.hash import check_hash
+from datetime import datetime
+
+
+def main():
+
+    recorder_df = read_table_from_sql("Fact_Recorder")
+    first_record = recorder_df.loc[1, "Number"]
+    df = read_table_from_sql("AlirezaReturns")
+    date = read_table_from_sql("Dim_Dates")
+
+
+    hash_df = read_table_from_sql("Fact_Hash")
+    first_record_hash = hash_df.shape[0]
+
+    filtered_df = preprocessing(df, 1)
+    print(filtered_df.head())
+    today = datetime.date(datetime.now())
+    time_range = date[date['Gregorian_Date'].astype(str) == str(today)]['Jalali_1'].str.replace("/", "").astype(int).values[0]
+
+
+    hash_df = check_hash(filtered_df, time_range, hash_df)
+    second_record_hash = hash_df.shape[0]
+
+    save_table_to_sql(hash_df, "Fact_Hash", if_exists="replace")
+    if first_record_hash != second_record_hash:
+        write_massege(first_record_hash, second_record_hash, 2)
+
+
+    return_flag=0
+
+    if not check_update(df.shape[0], recorder_df, 1):
+
+        map_df = read_table_from_sql("Bridge_Vistor_Customer")
+
+
+        ret_count_flag, ret_df_temp = check_count_flag(recorder_df, filtered_df, 1)
+
+
+        if ret_count_flag != 0:
+
+            ret_df = read_table_from_sql("Fact_Return")
+            return_flag = 1
+
+            uncheck_ret_df = ret_df_temp.copy()
+            uncheck_ret_df.loc[:, "Date"] = uncheck_ret_df["shamsi_date"].str[:7] + "/01"
+
+            visitor_ids = []
+
+            map_df_temp = pd.DataFrame(columns=['Customer_ID','visitor_id'])
+            for customer_id in uncheck_ret_df['Customer_ID']:
+                visitor_id, map_df = get_visitor_id(customer_id, map_df, map_df_temp)
+                visitor_ids.append(visitor_id)
+
+            uncheck_ret_df['visitor_id'] = visitor_ids
+
+            ret_df['Tarikh_komaki'] = ret_df['Tarikh_komaki'].astype(float).astype(int)
+            ret_df = ret_df[ret_df['Tarikh_komaki'] < 14040101]
+            ret_df['Tarikh_komaki'] = ret_df['Tarikh_komaki'].astype(str)
+
+
+            ret_df = pd.concat([ret_df, uncheck_ret_df])
+            save_table_to_sql(ret_df, "Fact_Return", "replace")
+
+            recorder_df.loc[1, "Number"] = ret_df_temp.shape[0]
+
+        save_table_to_sql(recorder_df, "Fact_Recorder", "replace")
+
+        sec_record = recorder_df.loc[1, "Number"]
+        write_massege(first_record, sec_record, 1)
+
+
+    if return_flag:
+
+
+        uncheck_df = merge_df_func(uncheck_ret_df, return_flag)
+        custom_df = read_table_from_sql("Dim_Custom")
+
+        new_customers = check_customer_id(uncheck_df['Customer_ID'].unique(), custom_df)
+
+
+        if new_customers:
+
+            custom_df_temp = pd.DataFrame(columns=['name', 'Customer_ID', 'Geographic_Customer_Group', 'Customer_Group'])
+
+            custom_df_temp[['Customer_ID', 'name']]= uncheck_df[uncheck_df['Customer_ID'].isin(new_customers)][
+                ['Customer_ID', 'custname']]
+
+
+            custom_df_temp.drop_duplicates(inplace=True)
+            custom_df_temp.reset_index(drop=True, inplace=True)
+
+            custom_df_temp['Customer_Group'] = 'مشهد'
+            custom_df_temp['Geographic_Customer_Group'] = '0'
+
+
+            save_table_to_sql(custom_df_temp, "Dim_Custom", "append")
+
+
+
+if __name__ == "__main__":
+    main()
